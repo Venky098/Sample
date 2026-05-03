3c41e7a005a84f60d97269890d0504528acb443b
YARRABATI VENKATESH NAIDU <69239877+Venky098@users.noreply.github.com>
2026-05-03 23:01:57 +0530

    Update output message in Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -5,7 +5,7 @@ public class Helloworld{
 
 public static void main(String[] args){
 
-  System.out.println("Hello world this is my first jenkins job connecting GitHub");
+  System.out.println("Hello world this is my first jenkins job connecting GitHub after modification");
 
 }
 
